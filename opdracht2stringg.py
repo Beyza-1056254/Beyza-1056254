be4088458a5a414854b12ecea95d88eabfa97252
Beyza-1056254 <1056254@hr.nl>
2022-09-14 13:45:40 +0200

    4 opdrachten toegevoegd

---
+++ opdracht2stringg.py
@@ -0,0 +1,19 @@
+ship = 'Titanic'
+print(ship)
+
+sentence = "He doesn't think it's a good idea to spend all his money on video games."
+print(sentence)
+
+paragraph = """Computer Technology means all designs, drawings, procedures including design, manufacturing, test and maintenance procedures), specifications, software (other than as described within the meaning of the term "Software" defined elsewhere herein), printed circuit board art work, integrated circuit masks, test equipment, tools, tixtures, documentation, training materials, and information, in whatever form, related to, useful, utilizable or necessary in the design, manufacture, test and/or maintenance of the computer system, or realte to any Technology Licenses. """
+print(paragraph)
+
+characters_in_paragraph = len(paragraph)
+print(f"{paragraph}\n\\The paragraph has {characters_in_paragraph} characters.")
+
+year = 1936
+inventor = "Alan Turing"
+name_of_machine = "automatic machine"
+
+invention = f'The Turing machine was inventor in {year} by {inventor}, who called it an "a-machine" ({name_of_machine}).'
+print(invention)
+type(invention)
